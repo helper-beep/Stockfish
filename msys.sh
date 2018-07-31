2d4e7c99e612f1fb6406622d4c59de64a4567b1c
helper-beep <40342587+helper-beep@users.noreply.github.com>
2018-07-31 17:34:49 +0300

    Changing...

--- msys.sh
+++ msys.sh
@@ -2,23 +2,22 @@
 
 export PATH=.:/mingw64/bin:/usr/local/bin:/mingw/bin:/bin
 export
-mkdir "$APPVEYOR_REPO_BRANCH"
 
 cd src
 
 make clean
 make profile-build COMP=mingw ARCH=x86-64
 strip stockfish.exe
-mv McB-96.exe.exe "../$APPVEYOR_REPO_BRANCH/mcbrain_x86_64.exe"
+mv McB-96.exe.exe "../C:/Projects/stockfish/McB-96_x86_64.exe"
 
 make clean
 make profile-build COMP=mingw ARCH=x86-64-modern
 strip stockfish.exe
-mv McB-96.exe.exe "../$APPVEYOR_REPO_BRANCH/McB-96_x86_64-modern.exe"
+mv McB-96.exe.exe "../C:/Projects/stockfish/McB-96_x86_64-modern.exe"
 
 make clean
 make profile-build COMP=mingw ARCH=x86-64-bmi2
 strip stockfish.exe
-mv McB-96.exe "../$APPVEYOR_REPO_BRANCH/McB-96.exe-x86_64-bmi2.exe"
+mv McB-96.exe "../C:/Projects/stockfish/McB-96.exe-x86_64-bmi2.exe"
 
 cd ..
