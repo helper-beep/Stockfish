14b67f4549613379a201f5e72bed2ee67cbae25c
helper-beep <40342587+helper-beep@users.noreply.github.com>
2018-08-01 21:21:15 +0300

    Update msys.sh

--- msys.sh
+++ msys.sh
@@ -8,16 +8,16 @@ cd src
 make clean
 make profile-build COMP=mingw ARCH=x86-64
 strip McB-96.exe
-mv McB-96.exe "../C:/Projects/stockfish/McB-96_x86_64.exe"
+mv McB-96.exe "../C:/Projects/stockfish/"
 
 make clean
 make profile-build COMP=mingw ARCH=x86-64-modern
 strip McB-96.exe
-mv McB-96.exe "../C:/Projects/stockfish/McB-96_x86_64-modern.exe"
+mv McB-96.exe "../C:/Projects/stockfish/"
 
 make clean
 make profile-build COMP=mingw ARCH=x86-64-bmi2
 strip McB-96.exe
-mv McB-96.exe "../C:/Projects/stockfish/McB-96-x86_64-bmi2.exe"
+mv McB-96.exe "../C:/Projects/stockfish/"
 
 cd ..
