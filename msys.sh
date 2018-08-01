0d33aa3ba7c7651e02455cae73b8b4137c3b3132
helper-beep <40342587+helper-beep@users.noreply.github.com>
2018-08-01 08:40:24 +0300

    AppVeyor BUILD: Passing

--- msys.sh
+++ msys.sh
@@ -8,16 +8,16 @@ cd src
 make clean
 make profile-build COMP=mingw ARCH=x86-64
 strip stockfish.exe
-mv McB-96.exe.exe "../C:/Projects/stockfish/McB-96_x86_64.exe"
+mv McB-96.exe "../C:/Projects/stockfish/McB-96_x86_64.exe"
 
 make clean
 make profile-build COMP=mingw ARCH=x86-64-modern
 strip stockfish.exe
-mv McB-96.exe.exe "../C:/Projects/stockfish/McB-96_x86_64-modern.exe"
+mv McB-96.exe "../C:/Projects/stockfish/McB-96_x86_64-modern.exe"
 
 make clean
 make profile-build COMP=mingw ARCH=x86-64-bmi2
 strip stockfish.exe
-mv McB-96.exe "../C:/Projects/stockfish/McB-96.exe-x86_64-bmi2.exe"
+mv McB-96.exe "../C:/Projects/stockfish/McB-96-x86_64-bmi2.exe"
 
 cd ..
